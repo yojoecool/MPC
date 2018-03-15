b83345e5f89a69ef32dd61e43b8920005ed9b28d
Bridges Penn <bridgespenn@Bridgess-MacBook.local>
2018-03-15 18:59:21 -0400

    upgrade to Swift 4

--- MPC/ViewController.swift
+++ MPC/ViewController.swift
@@ -11,17 +11,17 @@ import AVFoundation
 
 class ViewController: UIViewController, UITextFieldDelegate {
     var sounds = [AVAudioPlayer]()
-    var soundURLs = [NSURL]()
-    var metronome = try! AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("90bpm", ofType: "mp3")!))
+    var soundURLs = [URL]()
+    var metronome = try! AVAudioPlayer(contentsOf: URL(fileURLWithPath: Bundle.main.path(forResource: "90bpm", ofType: "mp3")!))
 
     @IBOutlet var pads: [UIButton]!
     @IBOutlet weak var BPM: UITextField!
     @IBOutlet weak var playMetro: UIButton!
     
-    @IBAction func playSound(sender: UIButton) {
+    @IBAction func playSound(_ sender: UIButton) {
         let tag = sender.tag
         
-        if (sounds[tag].playing){
+        if (sounds[tag].isPlaying){
             sounds[tag].stop()
         }
         
@@ -29,49 +29,49 @@ class ViewController: UIViewController, UITextFieldDelegate {
         sounds[tag].play()
     }
     
-    @IBAction func playMetro(sender: UIButton) {
+    @IBAction func playMetro(_ sender: UIButton) {
         metronome.stop()
         metronome.currentTime = 0
         
         if (playMetro.currentTitle == "Stop") {
-            playMetro.setTitle("Play", forState: .Normal)
+            playMetro.setTitle("Play", for: UIControlState())
         }
         else {
             if let newTempo = Float(BPM.text!) {
                 metronome.rate = (newTempo / 90.0)
                 metronome.play()
                 
-                playMetro.setTitle("Stop", forState: .Normal)
+                playMetro.setTitle("Stop", for: UIControlState())
             }
         }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let value = UIInterfaceOrientation.LandscapeLeft.rawValue
-        UIDevice.currentDevice().setValue(value, forKey: "orientation")
+        let value = UIInterfaceOrientation.landscapeLeft.rawValue
+        UIDevice.current.setValue(value, forKey: "orientation")
         
         // Do any additional setup after loading the view, typically from a nib.
-        soundURLs.append(NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("Clap1", ofType: "wav", inDirectory: "Sounds")!))
-        soundURLs.append(NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("Clap2", ofType: "wav", inDirectory: "Sounds")!))
-        soundURLs.append(NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("Clap1", ofType: "wav", inDirectory: "Sounds")!))
-        soundURLs.append(NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("Cymbals1", ofType: "wav", inDirectory: "Sounds")!))
-        soundURLs.append(NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("Hats1", ofType: "wav", inDirectory: "Sounds")!))
-        soundURLs.append(NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("Hats2", ofType: "wav", inDirectory: "Sounds")!))
-        soundURLs.append(NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("Kick1", ofType: "wav", inDirectory: "Sounds")!))
-        soundURLs.append(NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("Kick2", ofType: "wav", inDirectory: "Sounds")!))
-        soundURLs.append(NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("Kick3", ofType: "wav", inDirectory: "Sounds")!))
-        soundURLs.append(NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("Percs1", ofType: "wav", inDirectory: "Sounds")!))
-        soundURLs.append(NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("Percs2", ofType: "wav", inDirectory: "Sounds")!))
-        soundURLs.append(NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("Percs3", ofType: "wav", inDirectory: "Sounds")!))
-        soundURLs.append(NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("Percs4", ofType: "wav", inDirectory: "Sounds")!))
-        soundURLs.append(NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("Percs5", ofType: "wav", inDirectory: "Sounds")!))
-        soundURLs.append(NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("Percs6", ofType: "wav", inDirectory: "Sounds")!))
-        soundURLs.append(NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("Snare1", ofType: "wav", inDirectory: "Sounds")!))
-        soundURLs.append(NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("Snare2", ofType: "wav", inDirectory: "Sounds")!))
+        soundURLs.append(URL(fileURLWithPath: Bundle.main.path(forResource: "Clap1", ofType: "wav", inDirectory: "Sounds")!))
+        soundURLs.append(URL(fileURLWithPath: Bundle.main.path(forResource: "Clap2", ofType: "wav", inDirectory: "Sounds")!))
+        soundURLs.append(URL(fileURLWithPath: Bundle.main.path(forResource: "Clap1", ofType: "wav", inDirectory: "Sounds")!))
+        soundURLs.append(URL(fileURLWithPath: Bundle.main.path(forResource: "Cymbals1", ofType: "wav", inDirectory: "Sounds")!))
+        soundURLs.append(URL(fileURLWithPath: Bundle.main.path(forResource: "Hats1", ofType: "wav", inDirectory: "Sounds")!))
+        soundURLs.append(URL(fileURLWithPath: Bundle.main.path(forResource: "Hats2", ofType: "wav", inDirectory: "Sounds")!))
+        soundURLs.append(URL(fileURLWithPath: Bundle.main.path(forResource: "Kick1", ofType: "wav", inDirectory: "Sounds")!))
+        soundURLs.append(URL(fileURLWithPath: Bundle.main.path(forResource: "Kick2", ofType: "wav", inDirectory: "Sounds")!))
+        soundURLs.append(URL(fileURLWithPath: Bundle.main.path(forResource: "Kick3", ofType: "wav", inDirectory: "Sounds")!))
+        soundURLs.append(URL(fileURLWithPath: Bundle.main.path(forResource: "Percs1", ofType: "wav", inDirectory: "Sounds")!))
+        soundURLs.append(URL(fileURLWithPath: Bundle.main.path(forResource: "Percs2", ofType: "wav", inDirectory: "Sounds")!))
+        soundURLs.append(URL(fileURLWithPath: Bundle.main.path(forResource: "Percs3", ofType: "wav", inDirectory: "Sounds")!))
+        soundURLs.append(URL(fileURLWithPath: Bundle.main.path(forResource: "Percs4", ofType: "wav", inDirectory: "Sounds")!))
+        soundURLs.append(URL(fileURLWithPath: Bundle.main.path(forResource: "Percs5", ofType: "wav", inDirectory: "Sounds")!))
+        soundURLs.append(URL(fileURLWithPath: Bundle.main.path(forResource: "Percs6", ofType: "wav", inDirectory: "Sounds")!))
+        soundURLs.append(URL(fileURLWithPath: Bundle.main.path(forResource: "Snare1", ofType: "wav", inDirectory: "Sounds")!))
+        soundURLs.append(URL(fileURLWithPath: Bundle.main.path(forResource: "Snare2", ofType: "wav", inDirectory: "Sounds")!))
         
         for url in soundURLs {
-            sounds.append(try! AVAudioPlayer(contentsOfURL: url))
+            sounds.append(try! AVAudioPlayer(contentsOf: url))
         }
         
         metronome.enableRate = true
@@ -86,20 +86,20 @@ class ViewController: UIViewController, UITextFieldDelegate {
         // Dispose of any resources that can be recreated.
     }
 
-    override func shouldAutorotate() -> Bool {
+    override var shouldAutorotate : Bool {
         return true
     }
     
-    override func supportedInterfaceOrientations() -> UIInterfaceOrientationMask {
-        return [UIInterfaceOrientationMask.LandscapeLeft,UIInterfaceOrientationMask.LandscapeRight]
+    override var supportedInterfaceOrientations : UIInterfaceOrientationMask {
+        return [UIInterfaceOrientationMask.landscapeLeft,UIInterfaceOrientationMask.landscapeRight]
     }
     
-    func textFieldShouldReturn(textField: UITextField) -> Bool {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         return true
     }
     
-    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
     }
 }
